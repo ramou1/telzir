a2450759066ac1fde6746f431dfc5fe045c64844
Ramon Oliveira <ramonsoliveira1@hotmail.com>
2020-12-07 17:36:25 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  public calculo: any = [{ codOrigem: "", codDestino: "", tempoLigacao: "", plano: "" }];
+  disableButton: boolean = false; 
+  valores: boolean = false; 
+  precoNormal: number = 0.0;
+  precoPlano: number = 0.0;
+
+  ngOnInit(): void {    
+  }
+
+  alterarOrigem(valor) {
+    this.calculo.codOrigem = valor;
+  }
+
+  alterarDestino(valor) {
+    this.calculo.codDestino = valor;
+  }
+
+  calculoPlano(valor) {
+    if(this.calculo.tempoLigacao > this.calculo.plano) {
+      this.precoPlano = (this.calculo.tempoLigacao - this.calculo.plano) * ((0.1 * valor) + valor);
+    } 
+  }
+
+  calculoFinal() {
+    // this.disableButton = true;
+    this.valores = true;
+
+    if(this.calculo.codOrigem == "11") {
+      if(this.calculo.codDestino == "16") {
+        this.precoNormal = this.calculo.tempoLigacao * 1.9;
+        this.calculoPlano(1.9);
+      }
+      else if(this.calculo.codDestino == "17") {
+        this.precoNormal = this.calculo.tempoLigacao * 1.7;
+        this.calculoPlano(1.7);
+      }
+      else if(this.calculo.codDestino == "18") {
+        this.precoNormal = this.calculo.tempoLigacao * 0.9;
+        this.calculoPlano(0.9);
+      }
+    }
+
+    else if(this.calculo.codOrigem == "16") {
+      if(this.calculo.codDestino == "11") {
+        this.precoNormal = this.calculo.tempoLigacao * 2.9;
+        this.calculoPlano(2.9);
+      }
+    }
+
+    else if(this.calculo.codOrigem == "17") {
+      if(this.calculo.codDestino == "11") {
+        this.precoNormal = this.calculo.tempoLigacao * 2.7;
+        this.calculoPlano(2.7);
+      }
+    }
+
+    else if(this.calculo.codOrigem == "18") {
+      if(this.calculo.codDestino == "11") {
+        this.precoNormal = this.calculo.tempoLigacao * 1.9;
+        this.calculoPlano(1.9);
+      }
+    }
+
+    console.log("dados: ", this.calculo);
+  }
+
+}
